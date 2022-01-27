3824e55a839c4f00c999999ceac819936835ac7e
aaronkg1 <aaronkg1@gmail.com>
2022-01-27 17:17:42 +0000

    Made improvement to UI and implented timeouts to make rounds longer

--- RPSgame.js
+++ RPSgame.js
@@ -16,20 +16,41 @@ const overallScoresDisplay = document.createElement('p');
 const scoresContainer = document.querySelector('.scores');
 const displayUserScore = document.querySelector('#user');
 const displayCompScore = document.querySelector('#computer');
+const compImage = document.createElement('img');
+compImage.classList.add('shrinkimage');
+const displayCompImage = document.querySelector('#computerdisplay');
+const computerMessage = document.querySelector('#computermessage');
+const userMessage = document.querySelector('#yourmessage');
 
 scoresContainer.appendChild(displayChoices);
 scoresContainer.appendChild(outcomeDisplay);
 scoresContainer.appendChild(displayScores);
 scoresContainer.appendChild(overallScoresDisplay);
 
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    setTimeout(() => {button.disabled = false;}, 3300);    
+    })
+}
+
+
 
 
 buttons.forEach(button => button.addEventListener('click', userSelect));
 buttons.forEach(button => button.addEventListener('click', game));
+buttons.forEach(button => button.addEventListener('click', function () {
+    disableButtons();
+    this.classList.add('spin');
+    setTimeout(() => {this.classList.remove('spin');}, 1000);
+    this.classList.add('highlight-user');
+    setTimeout(() => {this.classList.remove('highlight-user');}, 3300);
+}));
 
 function userSelect(e) {
     if (e.target.value === "ROCK") {
         userChoice = choice[0];
+        
     }
 
     else if (e.target.value === "PAPER") {
@@ -46,9 +67,47 @@ function userSelect(e) {
 console.log("ROCK PAPER SCISSORS\n---------------------------------------------\nFIRST TO 5 WINS! to begin game type game();\n---------------------------------------------")
 
 function game() {
+    displayCompImage.appendChild(compImage);
+    
+    setTimeout(() => {
+        userMessage.textContent = '';
+        computerMessage.textContent =`Rock`;}, 200);
+    setTimeout(() => {compImage.setAttribute('src',`Images/Rock.png`);}, 200);
+    setTimeout(() => {compImage.setAttribute('src',``);}, 600);
+    setTimeout(() => {computerMessage.textContent =`. . .`;}, 600);
+    setTimeout(() => {computerMessage.textContent =`Paper`;}, 1000);
+    setTimeout(() => {compImage.setAttribute('src',`Images/Paper.png`);}, 1000);
+    setTimeout(() => {computerMessage.textContent =`. . .`;}, 1400);
+    setTimeout(() => {compImage.setAttribute('src',``);}, 1400);
+    setTimeout(() => {computerMessage.textContent =`Scissors!`;}, 1800);
+    setTimeout(() => {compImage.setAttribute('src',`Images/Scissors.png`);}, 1800);
+    setTimeout(() => {computerMessage.textContent =`. . .`;}, 2200);
+    setTimeout(() => {compImage.setAttribute('src',``);}, 2200);
+    setTimeout(() => {compImage.setAttribute('src',``);}, 2400);
+    setTimeout(() => {computerMessage.textContent =`.`;}, 2400);
+    setTimeout(() => {computerMessage.textContent =`. .`;}, 2500);
+    setTimeout(() => {computerMessage.textContent =`. . .`;}, 2600);
+    setTimeout(() => {computerMessage.textContent =`. . . .`;}, 2700);
+    setTimeout(() => {computerMessage.textContent =`. . . . .`;}, 2800);
+    setTimeout(() => {computerMessage.textContent =`.`;}, 2900);
+    setTimeout(() => {computerMessage.textContent =`. . `;}, 3000);
+    setTimeout(() => {computerMessage.textContent =`. . . `;}, 3100);
+    setTimeout(() => {computerMessage.textContent =`. . . . `;}, 3200);
+    setTimeout(() => {computerMessage.textContent =`. . . . .`;}, 3300);
+ 
+   setTimeout(() => {
     computerPlay();
-    displayChoices.textContent = `You picked ${userChoice} : Computer picked ${computerChoice}`;
-    checkResults();
+    compImage.setAttribute('src',`Images/${computerChoice}.png`);
+    compImage.setAttribute('src',`Images/${computerChoice}.png`);
+    userMessage.textContent = `You picked: ${userChoice}`;
+   computerMessage.textContent = `Computer picked: ${computerChoice}`;
+   checkResults();
+   displayUserScore.textContent = `User Score: ${userScore}`;
+   displayCompScore.textContent = `Computer Score: ${computerScore}`;}, 3400);
+    
+    
+    
+    
 
     if (checkScore()) {
         if (userScore >= 5) {
@@ -70,9 +129,7 @@ function game() {
     }
 
 
-  displayScores.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
-  displayUserScore.textContent = `User Score: ${userScore}`;
-  displayCompScore.textContent = `Computer Score: ${computerScore}`;
+  
 
 
   
@@ -98,38 +155,22 @@ function computerPlay() {
 
 function checkResults() {
     if (userChoice == computerChoice) {
-        outcomeDisplay.textContent = 'It was a tie!';
         return 0;
     }
 
-    else if (userChoice == choice[0] && computerChoice == choice[1]) {
-        outcomeDisplay.textContent = `${choice[1]} beats ${choice[0]}. Computer wins the round.`;
+    else if ((userChoice == choice[0] && computerChoice == choice[1]) || 
+    (userChoice == choice[1] && computerChoice == choice[2]) ||
+     (userChoice == choice[2] && computerChoice == choice[0])) {
         computerScore += 1;
+        displayCompScore.classList.toggle('add-Point');
+        setTimeout(() => {displayCompScore.classList.toggle('add-Point');}, 200);
     }
 
-    else if (userChoice == choice[0] && computerChoice == choice[2]) {
-        outcomeDisplay.textContent = `${choice[0]} beats ${choice[2]}. You win the round.`;
+    
+    else  {
         userScore += 1;
-    }
-
-    else if (userChoice == choice[1] && computerChoice == choice[0]) {
-        outcomeDisplay.textContent = `${choice[1]} beats ${choice[0]}. You win the round`;
-        userScore += 1;
-    }
-
-    else if (userChoice == choice[1] && computerChoice == choice[2]) {
-        outcomeDisplay.textContent = `${choice[2]} beats ${choice[1]}. Computer wins the round`;
-        computerScore += 1;
-    }
-
-    else if (userChoice == choice[2] && computerChoice == choice[0]) {
-        outcomeDisplay.textContent = `${choice[0]} beats ${choice[2]}. Computer wins the round.`;
-        computerScore += 1;
-    }
-
-    else if (userChoice == choice[2] && computerChoice == choice[1]) {
-        outcomeDisplay.textContent = `${choice[2]} beats ${choice[1]}. You win the round`;
-        userScore += 1;
+        displayUserScore.classList.toggle('add-Point');
+        setTimeout(() => {displayUserScore.classList.toggle('add-Point');}, 200);
 
     }
 }
@@ -142,7 +183,5 @@ function checkScore() {
 
     else return false;
 
-
 }
-
 
