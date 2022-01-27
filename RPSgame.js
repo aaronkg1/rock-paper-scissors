53897b20a52fa4da430c65e15b887489ed157fb8
aaronkg1 <aaronkg1@gmail.com>
2022-01-27 00:54:53 +0000

    Started to build UI for game.

--- RPSgame.js
+++ RPSgame.js
@@ -8,50 +8,74 @@ const games = {
     user: 0
 };
 
+const buttons = document.querySelectorAll('button');
+const displayChoices = document.createElement('p');
+const outcomeDisplay = document.createElement('p');
+const displayScores = document.createElement('p');
+const overallScoresDisplay = document.createElement('p');
+const scoresContainer = document.querySelector('.scores');
+const displayUserScore = document.querySelector('#user');
+const displayCompScore = document.querySelector('#computer');
+
+scoresContainer.appendChild(displayChoices);
+scoresContainer.appendChild(outcomeDisplay);
+scoresContainer.appendChild(displayScores);
+scoresContainer.appendChild(overallScoresDisplay);
+
+
+
+buttons.forEach(button => button.addEventListener('click', userSelect));
+buttons.forEach(button => button.addEventListener('click', game));
+
+function userSelect(e) {
+    if (e.target.value === "ROCK") {
+        userChoice = choice[0];
+    }
+
+    else if (e.target.value === "PAPER") {
+        userChoice = choice[1];
+    }
+
+    else if (e.target.value === "SCISSORS") {
+        userChoice = choice[2];
+    }
+};
+
+
 
 console.log("ROCK PAPER SCISSORS\n---------------------------------------------\nFIRST TO 5 WINS! to begin game type game();\n---------------------------------------------")
 
 function game() {
-
-    userPlay();
-    
-    if (userChoice == choice[0] || userChoice == choice[1] || userChoice == choice[2])
-    {
-        
-    }  else if (userChoice == '' || userChoice == null) {
-        console.log('---------------------------------------------\nPlease make a valid selection\n---------------------------------------------\n');
-        game();
-    }
     computerPlay();
-    console.log(`You picked ${userChoice} : Computer picked ${computerChoice}\n---------------------------------------------`);
+    displayChoices.textContent = `You picked ${userChoice} : Computer picked ${computerChoice}`;
     checkResults();
 
     if (checkScore()) {
         if (userScore >= 5) {
-            console.log("\n \n \nCongratulations! You beat the computer :)\n \n \n---------------------------------------------\nTo play again type game();");
             games.user += 1;
             userScore = 0;
             computerScore = 0;
-            console.log(`Overall scores\n--------------------------------------------- \nUser: ${games.user}  Computer: ${games.computer}`);
+            overallScoresDisplay.textContent = `Overall scores User: ${games.user}  Computer: ${games.computer}`;
             return;
         }
 
         else if (computerScore >= 5) {
-            console.log("\n \n \nThe computer won...Game over :(\n \n \n---------------------------------------------\nWant to try again? Just type game();");
             games.computer += 1;
             userScore = 0;
             computerScore = 0;
-            console.log(`Overall scores\n--------------------------------------------- \nUser: ${games.user}  Computer: ${games.computer}`);
+            overallScoresDisplay.textContent = `Overall scores User: ${games.user}  Computer: ${games.computer}`;
             return;
         }
 
     }
 
 
-    console.log(`---------------------------------------------\nYour score: ${userScore}\n---------------------------------------------\nComputer score: ${computerScore}\n---------------------------------------------\n`);
+  displayScores.textContent = `Your score: ${userScore} Computer score: ${computerScore}`;
+  displayUserScore.textContent = `User Score: ${userScore}`;
+  displayCompScore.textContent = `Computer Score: ${computerScore}`;
 
 
-    game();
+  
 
 }
 
@@ -68,68 +92,43 @@ function computerPlay() {
         computerChoice = choice[2];
 }
 
-function userPlay() {
-    userChoice = prompt('Please choose Rock, Paper or Scissors!')
-    if (userChoice == null) {
-        return;
-    }
-    let lowerCaseUserChoice = userChoice.toLowerCase();
 
-    if (lowerCaseUserChoice == "rock") {
-        userChoice = choice[0];
-        
-    }
-
-    else if (lowerCaseUserChoice == "paper") {
-        userChoice = choice[1];
-        
-    }
-
-    else if (lowerCaseUserChoice == "scissors") {
-        userChoice = choice[2];
-        
-    }
-
-    else if (lowerCaseUserChoice == '' || lowerCaseUserChoice == null) {
-
-    }
-}
 
 
 
 function checkResults() {
     if (userChoice == computerChoice) {
-        console.log('It was a tie!');
+        outcomeDisplay.textContent = 'It was a tie!';
         return 0;
     }
 
     else if (userChoice == choice[0] && computerChoice == choice[1]) {
-        console.log(`${choice[1]} beats ${choice[0]}. Computer wins the round.`);
+        outcomeDisplay.textContent = `${choice[1]} beats ${choice[0]}. Computer wins the round.`;
         computerScore += 1;
     }
 
     else if (userChoice == choice[0] && computerChoice == choice[2]) {
-        console.log(`${choice[0]} beats ${choice[2]}. You win the round.`);
+        outcomeDisplay.textContent = `${choice[0]} beats ${choice[2]}. You win the round.`;
         userScore += 1;
     }
 
     else if (userChoice == choice[1] && computerChoice == choice[0]) {
-        console.log(`${choice[1]} beats ${choice[0]}. You win the round`);
+        outcomeDisplay.textContent = `${choice[1]} beats ${choice[0]}. You win the round`;
         userScore += 1;
     }
 
     else if (userChoice == choice[1] && computerChoice == choice[2]) {
-        console.log(`${choice[2]} beats ${choice[1]}. Computer wins the round`)
+        outcomeDisplay.textContent = `${choice[2]} beats ${choice[1]}. Computer wins the round`;
         computerScore += 1;
     }
 
     else if (userChoice == choice[2] && computerChoice == choice[0]) {
-        console.log(`${choice[0]} beats ${choice[2]}. Computer wins the round.`)
+        outcomeDisplay.textContent = `${choice[0]} beats ${choice[2]}. Computer wins the round.`;
         computerScore += 1;
     }
 
     else if (userChoice == choice[2] && computerChoice == choice[1]) {
-        console.log(`${choice[2]} beats ${choice[1]}. You win the round`)
+        outcomeDisplay.textContent = `${choice[2]} beats ${choice[1]}. You win the round`;
         userScore += 1;
 
     }
@@ -145,4 +144,5 @@ function checkScore() {
 
 
 }
+
 
